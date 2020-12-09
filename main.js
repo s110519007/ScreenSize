5776aad0ccb3d6c36aba999aa0d47bae202ab146
s110519007 <s110519007@stu.ntue.edu.tw>
2020-12-09 16:36:47 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
     var w=$(window).width();
     var h=$(window).height();
-    $(".container").text("寬度: "+w+"px, 高度: "+h+"px");
+    $('.container').html("寬度: "+w+"px, 高度: "+h+"px");
+    $(window).resize(function () { 
+        w=$(window).width();
+        h=$(window).height();
+        $(".container").text("寬度: "+w+"px, 高度: "+h+"px");
+    });
 });
