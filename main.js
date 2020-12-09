ef49f2d2711c123c92a50866c323a35ebd60a076
s110519007 <s110519007@stu.ntue.edu.tw>
2020-12-09 17:07:26 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // 隱藏連結列
     if(document.documentElement.scrollHeight <= document.documentElement.clientHeight) {
         bodyTag = document.getElementsByTagName('body')[0];
         bodyTag.style.height = document.documentElement.clientWidth / screen.width * screen.height + 'px';
